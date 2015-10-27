7e63eb2db516fd2b30827c030fbb0d0778e45f6e
root <root@vm-141-40-254-19.cloud.mwn.de>
2015-10-27 15:59:40 +0100

    updates

--- simple_run_lasttest.sh
+++ simple_run_lasttest.sh
@@ -7,8 +7,9 @@ set -x
 num_server=$1
 jmx_file="/root/jmeterTutorials/simple_lasttest_tp_0.jmx"
 data_dir="/root"
+work_dir="/root/testlogs"
 clear;
-/bin/bash driver_swarm.sh -s $jmx_file -d $data_dir -n $num_server
+/bin/bash driver_swarm.sh -s $jmx_file -d $data_dir -n $num_server -w $work_dir
 
 # turn of debug mode
 set +x
