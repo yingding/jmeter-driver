9561b3c3e15d81c8de3d5df9af3505da214526ba
root <root@vm-141-40-254-8.cloud.mwn.de>
2015-10-27 13:51:08 +0100

    add simple_run_lasttest.sh script

---
+++ simple_run_lasttest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# it seam like the ubuntu dash(default shell) 
+# has some problem with function parentheses.
+# Use /bin/bash to indicate the right intepreter
+# turn on debug mode
+set -x
+num_server=$1
+jmx_file="/root/jmeterTutorials/simple_lasttest_tp_0.jmx"
+data_dir="/root"
+clear;
+/bin/bash driver_swarm.sh -s $jmx_file -d $data_dir -n $num_server
+
+# turn of debug mode
+set +x
+exit 0
