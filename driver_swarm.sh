908cefe6490a7559308bc5b687acd1c245d9db3a
root <root@vm-141-40-254-19.cloud.mwn.de>
2015-10-27 16:19:16 +0100

    bug fixing

--- driver_swarm.sh
+++ driver_swarm.sh
@@ -256,8 +256,6 @@ mkdir -p ${LOGDIR}
 ## start the docker client on the local master nodes
 docker -H tcp://0.0.0.0:2375 run --cidfile ${LOGDIR}/cid \
 				-d \
-#                                -p ${HOST_READ_PORT}:1099 \
-#                                -p ${HOST_WRITE_PORT}:60000 \
                                 -v ${LOGDIR}:/logs \
 				-v ${DATADIR}:/input_data \
 				-v $(dirname ${JMX_SCRIPT}):/scripts \
